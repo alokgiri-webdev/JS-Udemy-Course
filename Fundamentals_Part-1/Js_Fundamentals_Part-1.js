2ee1e5406b4304667d61f4956456c57976d1b45b
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2024-06-15 17:48:09 +0530

    Add hello Alok

--- Fundamentals_Part-1/Js_Fundamentals_Part-1.js
+++ Fundamentals_Part-1/Js_Fundamentals_Part-1.js
@@ -2,5 +2,5 @@
 console.log('Hello World');
 const calcAge = birthYear => 2037 - birthYear;
 console.log(calcAge(1992));
-
+console.log('Hello Alok');
 //--------------------------------------------------------FUNDAMENTALS-1----------------------------------------------------------------//
