081c563980376019afff509aa44be918495e2ce2
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2024-06-15 17:25:00 +0530

    Add Fundamentals Part-1

---
+++ Fundamentals_Part-1/Js_Fundamentals_Part-1.js
@@ -0,0 +1,5 @@
+'use strict';
+// console.log('Hello World');
+// const calcAge = birthYear => 2037 - birthYear;
+// console.log(calcAge(1992));
+//--------------------------------------------------------FUNDAMENTALS-1----------------------------------------------------------------//
--- javaScript_Udemy.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-console.log('Hello World');
-const calcAge = birthYear => 2037 - birthYear;
-console.log(calcAge(1992));
