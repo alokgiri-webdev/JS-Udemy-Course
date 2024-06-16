e6100bbfbad70fde091b9d6d33b09af1af0cf15b
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2024-06-16 09:59:30 +0530

    commented

--- Fundamentals_Part-1/Js_Fundamentals_Part-1.js
+++ Fundamentals_Part-1/Js_Fundamentals_Part-1.js
@@ -1,6 +1,6 @@
 'use strict';
-console.log('Hello World');
-const calcAge = birthYear => 2037 - birthYear;
-console.log(calcAge(1992));
-console.log('Hello Alok');
+// console.log('Hello World');
+// const calcAge = birthYear => 2037 - birthYear;
+// console.log(calcAge(1992));
+// console.log('Hello Alok');
 //--------------------------------------------------------FUNDAMENTALS-1----------------------------------------------------------------//
